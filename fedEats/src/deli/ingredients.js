087393716141f45346bb4f35d5ac3df4c3c3a25d
Conner Matson <connermatson@ConnerMatson.local>
2019-09-17 13:59:41 -0500

    Makeshift Database - Deli[ingredients]

---
+++ fedEats/src/deli/ingredients.js
@@ -0,0 +1,67 @@
+export const bread = ["Sourdough", "Wheat", "Ciabatta", "Honey Wheat"];
+export const tortilla = [
+	"White Flour",
+	"Wheat",
+	"Honey Wheat",
+	"Tomato Basil",
+	"Spinach"
+];
+export const protein = [
+	"Ham",
+	"Roast Beef",
+	"Turkey",
+	"Salami",
+	"Tuna Salad",
+	"Grilled Chicken",
+	"Breaded Chicken",
+	"Pepperoni",
+	"Chicken Salad"
+];
+export const cheese = [
+	"Cheddar",
+	"American",
+	"Swiss",
+	"Provolone",
+	"Pepper Jack",
+	"Parmesan"
+];
+export const veggies = [
+	"Shredded Lettuce",
+	"Leaf Lettuce",
+	"Tomatoes",
+	"Red Onions",
+	"Jalapeños",
+	"Mixed Peppers",
+	"Banana Peppers",
+	"Mushrooms",
+	"Sprouts",
+	"Cucumbers",
+	"Pickles"
+];
+export const condiments = [
+	"Mayonnaise",
+	"Spicy Mayonnaise",
+	"Ranch Dressing",
+	"Buffalo Sauce",
+	"Sriracha",
+	"Mustard",
+	"Honey Mustard",
+	"Spicy Brown Mustard",
+	"Hummus"
+];
+export const extras = ["Pickle", "Carrots", "Avocado"];
+export const pickupTime = [
+	"11:00",
+	"11:15",
+	"11:30",
+	"11:45",
+	"12:00",
+	"12:15",
+	"12:30",
+	"12:45",
+	"1:00",
+	"1:15",
+	"1:30",
+	"1:45",
+	"2:00"
+];
