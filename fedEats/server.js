c1f3b52b4825fb5d3cd7ceabdce3b70f8411b475
Christopher Seagraves <christopher.seagraves@users.noreply.github.com>
2019-09-24 14:05:03 -0500

    Query Function in server.js

--- fedEats/server.js
+++ fedEats/server.js
@@ -1,13 +1,33 @@
 const express = require('express');
+const mysql = require("mysql");
 const app = express();
 const port = process.env.PORT || 5000;
+const query = "SELECT * FROM condiments";
+
+
+const connection = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "PASSWORD HERE",
+  database: "fedeats"
+});
+
+connection.connect(function(err) {});
 
 //show that server is running
 app.listen(port, () => console.log(`Listening on port ${port}`));
 
+function runQuery(query) {
+  app.get('/expressBackend', (req, res) => {
+    connection.query(query, function(err, data) {
+      return data;
+    });
+  });
+};
+
 //create GET route
 app.get('/expressBackend', (req, res) => {
   res.send( {
-    express: 'EXPRESS/REACT'
+    express: new Date()
   });
 });
