fdf2bb31eba14a0a667f925a0ceaca717103fd96
Christopher Seagraves <christopher.seagraves@users.noreply.github.com>
2019-09-26 12:48:46 -0500

    Update server.js

--- fedEats/server.js
+++ fedEats/server.js
@@ -2,14 +2,14 @@ const express = require('express');
 const mysql = require("mysql");
 const app = express();
 const port = process.env.PORT || 5000;
-const query = "SELECT * FROM condiments";
+const query = "SELECT * FROM test";
 
 
 const connection = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "PASSWORD HERE",
-  database: "fedeats"
+  host: "https://www.math-cs.ucmo.edu/phpmyadmin/",
+  user: "F19fedres2",
+  password: "F19pw$",
+  database: "F19fedres2"
 });
 
 connection.connect(function(err) {});
@@ -20,10 +20,12 @@ app.listen(port, () => console.log(`Listening on port ${port}`));
 function runQuery(query) {
   app.get('/expressBackend', (req, res) => {
     connection.query(query, function(err, data) {
+      console.log(data);
       return data;
     });
   });
 };
+//console.log(runQuery(query))
 
 //create GET route
 app.get('/getDate', (req, res) => {
