5e9b570c6453a7a4138dff64460cdd7fd2889d8e
Conner Matson <connermatson@ConnerMatson.local>
2019-09-30 14:46:47 -0500

    consolidated Items Component

--- MERN/client/src/Admin.js
+++ MERN/client/src/Admin.js
@@ -3,125 +3,59 @@ import {Col, Row, Container} from 'reactstrap';
 
 import './App.css';
 class Admin extends React.Component {
-
+  constructor(props) {
+    super();
+  }
   state = {
-    breads: [], 
-    tortillas: [], 
-    proteins: [],
-    cheeses: [], 
-    veggies: [],
-    condiments: [], 
-    extras: [],
-    specials: [], 
-    
-    newCheese: {
-      cheese_id: null, 
+    items: [], 
+    newItem: {
+      item_id: null, 
       name: '', 
       price: null
     }, 
-    cheeseToRemove: {
-      cheese_id: null
-    },
-    newVeggie: {
-      veggie_id: null, 
-      name: '', 
-      price: null
-    }, 
-    veggieToRemove: {
-      veggie_id: null
-    }, 
-    newBread: {
-      sandwich_id: null, 
-      name: '', 
-      price: null
-    }, 
-    breadToRemove: {
-      sandwich_id: null
-    },
-    newTortilla: {
-      tortilla_id: null, 
-      name: '', 
-      price: null
-    }, 
-    tortillaToRemove: {
-      tortilla_id: null
-    },
-    newProtein: {
-      protein_id: null, 
-      name: '', 
-      price: null
-    }, 
-    proteinToRemove: {
-      tortilla_id: null
-    },
-    newCondiment: {
-      condiments_id: null, 
-      name: '', 
-      price: null
-    }, 
-    condimentToRemove: {
-      condiments_id: null
-    },
-    newExtra: {
-      extras_id: null, 
-      name: '', 
-      price: null
-    }, 
-    extraToRemove: {
-      extras_id: null
-    },
-    newSpecial: {
-      main_id: null, 
-      name: '', 
-      price: null
-    }, 
-    specialToRemove: {
-      main_id: null
-    },
+    itemToRemove: {
+      item_id: null
+    }
   };
 
   componentDidMount() {
-     this.interval = setInterval(() => this.getIngredients(), 3000);
+     this.interval = setInterval(() => this.getItems(), 3000);
   }
 
   componentWillUnmount() {
     clearInterval(this.interval);
   }
 
-  getIngredients = () => {
-    this.getCheeses(); 
-    this.getVeggies();
-  }
 
-  //CHEESES
-  getCheeses = () => {
-    fetch('http://localhost:5000/cheeses')
+  //ITEMS
+  getItems = () => {
+    fetch(`http://localhost:5000/${this.props.category}`)
     .then(response => response.json())
-    .then(response => this.setState({ cheeses : response.data}))
+    .then(response => this.setState({ items : response.data}))
     .catch(err => console.error(err))
   };
-  addCheese = async () => {
-    const { newCheese } = this.state;
-    fetch(`http://localhost:5000/cheeses/add?cheese_id=${newCheese.cheese_id}&name=${newCheese.name}&price=${newCheese.price}`)
-    .then(this.getCheeses)
+  addItem = async () => {
+    const { newItem } = this.state;
+    fetch(`http://localhost:5000/${this.props.category}/add?name=${newItem.name}&price=${newItem.price}`)
+    .then(this.getItems)
     .then()
     .catch(err => console.error(err))
   };
-  deleteCheese = async () => {
-    const { cheeseToRemove } = this.state; 
-    fetch(`http://localhost:5000/cheeses/delete?cheese_id=${cheeseToRemove.cheese_id}`)
-    .then(this.getCheeses)
-    .then(console.log(`removed: ${cheeseToRemove}`))
+  deleteItem = async () => {
+    const { itemToRemove } = this.state; 
+    fetch(`http://localhost:5000/${this.props.category}/delete?name=${itemToRemove.name}`)
+    .then(this.getItems)
+    .then(console.log(`removed: ${itemToRemove}`))
     .catch(err => console.error(err))
   };
-  renderCheese = (cheese) => {
-    const {cheeseToRemove} = this.state;
+  renderItem = (item) => {
+    const {itemToRemove} = this.state;
     return (
-    <div key={cheese.cheese_id}>
-      <span>Cheese: {cheese.name} -  ${cheese.price.toFixed(2)}</span>
+    <div key={item.name}>
+      <span>${this.props.category}: {item.name} -  ${item.price.toFixed(2)}</span>
       <button onClick={(e) =>  { 
-        this.setState({cheeseToRemove:{...cheeseToRemove, cheese_id: cheese.cheese_id}});
-        this.deleteCheese();
+        this.setState({itemToRemove:{...itemToRemove, name: item.name}});
+        this.deleteItem();
       }}>delete</button>
       </div>
     );
@@ -130,7 +64,7 @@ class Admin extends React.Component {
 
 
   render() {  
-    const {cheeses, newCheese, veggies, newVeggie} = this.state;
+    const {items, newItem} = this.state;
     return (
       <div className="App">
         <header>
@@ -143,29 +77,15 @@ class Admin extends React.Component {
         </header>
 
         <div style={{border: '1px solid black'}} className='container'>
-          <h4>Cheeses</h4>
-          {cheeses.map(this.renderCheese)}
+          <h4>{this.props.category}</h4>
+          {items.map(this.renderItem)}
         </div>
         <div>
-          <input placeholder="ID" onChange={e => this.setState({newCheese: {...newCheese, cheese_id: e.target.value}})}/>
-          <input placeholder="Name" onChange={e => this.setState({newCheese: {...newCheese, name: e.target.value}})}/>
-          <input placeholder="Price" onChange={e => this.setState({newCheese: {...newCheese, price: e.target.value}})}/>
+          <input placeholder="Name" onChange={e => this.setState({newItem: {...newItem, name: e.target.value}})}/>
+          <input placeholder="Price" onChange={e => this.setState({newItem: {...newItem, price: e.target.value}})}/>
           <button onClick={() => {
-               this.addCheese()
-          }}>Add Cheese</button>
-        </div>
-
-        <div style={{border: '1px solid black'}}>
-          <h4>Veggies</h4>
-          {veggies.map(this.renderVeggie)}
-        </div>
-        <div>
-          <input placeholder="ID" onChange={e => this.setState({newVeggie: {...newVeggie, veggie_id: e.target.value}})}/>
-          <input placeholder="Name" onChange={e => this.setState({newVeggie: {...newVeggie, name: e.target.value}})}/>
-          <input placeholder="Price" onChange={e => this.setState({newVeggie: {...newVeggie, price: e.target.value}})}/>
-          <button onClick={() => {
-               this.addVeggie()
-          }}>Add Vegetable</button>
+               this.addItem()
+          }}>Add ${this.props.category}</button>
         </div>
       </div>
     );
