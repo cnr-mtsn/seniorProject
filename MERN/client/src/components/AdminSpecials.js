2b9483591fde2da56b5cdb986f4811478970c088
Conner Matson <connermatson@ConnerMatson.local>
2019-11-17 18:16:41 -0600

    special Card Image fixed

--- MERN/client/src/components/AdminSpecials.js
+++ MERN/client/src/components/AdminSpecials.js
@@ -13,11 +13,13 @@ import {
 } from "reactstrap";
 
 import ItemsCarousel from "react-items-carousel";
-// import pastrami from "./media/pastrami.png";
+import pastrami from "../media/pastrami.png";
 // import chickenBaconRanch from "./media/chickenBaconRanch.png";
 // import southwestWrap from "./media/southwestChickenWrap.png";
-// import roastBeef from "./media/roastBeef.png";
 import shrimpWrap from "../media/shrimpWrap.png";
+import roastBeef from '../media/roastBeef.png';
+import chickenBaconRanch from '../media/chickenBaconRanch.png';
+import southwestChickenWrap from '../media/southwestChickenWrap.png';
 import "../App.css";
 
 const AdminSpecials = () => {
@@ -48,7 +50,37 @@ const AdminSpecials = () => {
 
   const renderCards = (special) => {
 	
-	  const fixedPrice = `$${special.price.toFixed(2)}`;
+    const fixedPrice = `$${special.price.toFixed(2)}`;
+    var imgImport;
+
+    // if(special.imageName === 'roastBeef') {
+    //   imgImport = roastBeef;
+    // } else if(special.imageName === 'shrimpWrap') {
+    //   imgImport = shrimpWrap;
+    // } else if(special.imageName === 'chickenBaconRanch') {
+    //   imgImport = chickenBaconRanch;
+    // } else if(special.imageName === 'southwestChickenWrap') {
+    //   imgImport = southwestChickenWrap;
+    // } else {
+    //   img
+    // }
+    switch (special.imageName) {
+			case "roastBeef":
+				imgImport = roastBeef;
+				break;
+			case "shrimpWrap":
+				imgImport = shrimpWrap;
+				break;
+			case "chickenBaconRanch":
+				imgImport = chickenBaconRanch;
+				break;
+			case "southwestChickenWrap":
+				imgImport = southwestChickenWrap;
+				break;
+      default:
+        imgImport = pastrami;
+		}
+    
 
     const updateSpecial = async () => {
       fetch(`http://localhost:5000/specials/update?name=${special.name}&newName=${newName}&price=${newPrice}&description=${newDesc}&healthPoints=${newHP}`)
@@ -64,94 +96,92 @@ const AdminSpecials = () => {
     };
 
     return (
-      <div key={special.main_id} style={{ height: "200" }}>
-        <Card>
-          <CardImg top width="100%" src={shrimpWrap} alt={special.name} />
-          <CardBody>
-            <Row>
-              <Col>
-                <InputGroup>
-                  <InputGroupAddon
-                    className="specialUpdateAddon"
-                    addonType="prepend"
-                  >
-                    Name
-                  </InputGroupAddon>
-                  <Input
-                    name="special.name"
-                    placeholder={special.name}
-                    onChange={e => {
-                      setNewName(e.target.value);
-                    }}
-                  ></Input>
-                </InputGroup>
-              </Col>
-            </Row>
-            <br></br>
-            <Row>
-              <Col>
-                <InputGroup>
-                  <InputGroupAddon
-                    className="specialUpdateAddon"
-                    addonType="prepend"
-                  >
-                    Price
-                  </InputGroupAddon>
-                  <Input
-                    name="specialPrice"
-                    placeholder={fixedPrice}
-                    onChange={e => {
-                      setNewPrice(e.target.value);
-                    }}
-                  ></Input>
-                </InputGroup>
-              </Col>
-              <Col>
-                <InputGroup>
-                  <InputGroupAddon
-                    className="specialUpdateAddon"
-                    addonType="prepend"
-                  >
-                    H.P.
-                  </InputGroupAddon>
-                  <Input
-                    name="specialHP"
-                    placeholder={special.health_points}
-                    onChange={e => {
-                      setNewHP(e.target.value);
-                    }}
-                  ></Input>
-                </InputGroup>
-              </Col>
-            </Row>
-			<br></br>
-            <Row>
-              <Col>
-                <Input
-                  style={{ marginTop: "-8px" }}
-                  type="textarea"
-                  overflow="hidden"
-                  name="specialDesc"
-                  placeholder={special.description}
-                  onChange={e => {
-                    setNewDesc(e.target.value);
-                  }}
-                ></Input>
-              </Col>
-            </Row>
-          </CardBody>
-          <CardFooter>
-            <Button
-              color="primary"
-              value="Update"
-              onClick={handleSpecialUpdate}
-            >
-              Apply Changes
-            </Button>
-          </CardFooter>
-        </Card>
-      </div>
-    );
+			<div key={special.main_id} style={{ height: "200" }}>
+				<Card>
+					<CardImg
+						top
+						width='100%'
+						// src={`../media/${special.imageName}.png`}
+						src={imgImport}
+						alt={special.name}
+					/>
+					<CardBody>
+						<Row>
+							<Col>
+								<InputGroup>
+									<InputGroupAddon
+										className='specialUpdateAddon'
+										addonType='prepend'>
+										Name
+									</InputGroupAddon>
+									<Input
+										name='special.name'
+										placeholder={special.name}
+										onChange={e => {
+											setNewName(e.target.value);
+										}}></Input>
+								</InputGroup>
+							</Col>
+						</Row>
+						<br></br>
+						<Row>
+							<Col>
+								<InputGroup>
+									<InputGroupAddon
+										className='specialUpdateAddon'
+										addonType='prepend'>
+										Price
+									</InputGroupAddon>
+									<Input
+										name='specialPrice'
+										placeholder={fixedPrice}
+										onChange={e => {
+											setNewPrice(e.target.value);
+										}}></Input>
+								</InputGroup>
+							</Col>
+							<Col>
+								<InputGroup>
+									<InputGroupAddon
+										className='specialUpdateAddon'
+										addonType='prepend'>
+										H.P.
+									</InputGroupAddon>
+									<Input
+										name='specialHP'
+										placeholder={special.health_points}
+										onChange={e => {
+											setNewHP(e.target.value);
+										}}></Input>
+								</InputGroup>
+							</Col>
+						</Row>
+						<br></br>
+						<Row>
+							<Col>
+								<Input
+									style={{ marginTop: "-8px" }}
+									type='textarea'
+									overflow='hidden'
+									name='specialDesc'
+									placeholder={special.description}
+									onChange={e => {
+										setNewDesc(e.target.value);
+									}}></Input>
+							</Col>
+						</Row>
+					</CardBody>
+					<CardFooter>
+						<Button
+							color='primary'
+							value='Update'
+							onClick={handleSpecialUpdate}>
+							Apply Changes
+						</Button>
+					</CardFooter>
+				</Card>
+			</div>
+		);
   };
 
   return (
