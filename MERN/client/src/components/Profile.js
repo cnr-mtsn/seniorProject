671ba4c34a498b7c95e215ada761c9d32052c6ce
Conner Matson <connermatson@ConnerMatson.hsd1.mo.comcast.net>
2019-11-20 19:53:52 -0600

    profile page works, just need to pull orders and display stars according to health_point_avg

--- MERN/client/src/components/Profile.js
+++ MERN/client/src/components/Profile.js
@@ -1,7 +1,7 @@
 import React, {useState} from 'react';
 import Header from '../components/Header';
 
-import { FaCoins } from 'react-icons/fa';
+import { FaUserTie, FaStar, FaStarHalf } from 'react-icons/fa';
 
 
 function Profile(props) {
@@ -17,28 +17,64 @@ function Profile(props) {
 	const [userData, setUserData] = useState(getUserStats());
 
     return (
-		<div className="profileWrapper">
-
-			<div className="profileHeader">
-				<Header user={user} view={userData.view}/>
-			</div>
-
-			<div className="profileSide">
-				<div className="profileSidePicAndStars">
-					<div className="profileSidePic">pic</div>
-					<div className="profileSideStars">stars</div>
+			<div className='profileWrapper'>
+				<div className='profileHeader'>
+					<Header user={user} view={userData.view} />
 				</div>
-				<div className="profileSideLinks">Links</div>
-				<div className="profileSideFooter">Email, ID</div>
-			</div>
 
-			<div className="profileBody">
-				<div className="profileBodyLabels">Labels</div>
-				<div className="profileBodyData">BodyData</div>
-			</div>
+				<div className='profileSide'>
+					<div className='profileSidePicAndStars'>
+						<div className='profileSidePic'>
+							<div className='profileSidePicInner'>
+								<FaUserTie size={256} />
+							</div>
+						</div>
+						<div className='profileSideStars'>
+							<div className='profileSideStarsInner'>
+								<FaStar size={48} />
+								<FaStar size={48} />
+								<FaStar size={48} />
+								<FaStarHalf size={48} />
+							</div>
+						</div>
+					</div>
+					<div className='profileSideLinks'>
+						<div className='profileSideLinksOrders'>
+							<h5>My Orders</h5>
+						</div>
+						<div className='profileSideLinksFavs'>
+							<h5>Favorites</h5>
+						</div>
+					</div>
 
-		</div>
-	);
+					<div className='profileSideFooter'>
+						<div className='profileSideFooterEmail'>
+							<h6>{userData.email}</h6>
+						</div>
+						<div className='profileSideFooterID'>
+							<h6>ID: {userData.user_id}</h6>
+						</div>
+					</div>
+				</div>
+
+				<div className='profileBody'>
+					<div className='profileBodyLabels'>
+						<div className='profileBodyLabelName'>Name</div>
+						<div className='profileBodyLabelPrice'>Price</div>
+						<div className='profileBodyLabelDate'>Date</div>
+						<div className='profileBodyLabelItems'>Items</div>
+					</div>
+					<div className='profileBodyData'>
+						<div className='profileBodyItem'>
+							<div className='profileBodyDataName'>Roast Beef</div>
+							<div className='profileBodyDataPrice'>$4.20</div>
+							<div className='profileBodyDataDate'>11/20/2019</div>
+							<div className='profileBodyDataItems'>View Order</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		);
 }
 
 
